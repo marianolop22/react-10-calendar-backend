f150463f858b3ba8a631f438a2582405c1abbf5f
Mariano Lopez <marianolop22@yahoo.com.ar>
2020-10-24 11:40:09 -0300

    agregado del proyecto react

---
+++ public/precache-manifest.c497fd888217d950ab6c06c8a5fad99a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff838f9483ca97769a8fd497a8d800b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f92b3c3e43acab87b57e",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "ab29cadb9162ccbdafae",
+    "url": "/static/css/main.e9611fe3.chunk.css"
+  },
+  {
+    "revision": "f92b3c3e43acab87b57e",
+    "url": "/static/js/2.c382d0e7.chunk.js"
+  },
+  {
+    "revision": "da64645f1a216f6b9bfcff639bf8d4fd",
+    "url": "/static/js/2.c382d0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab29cadb9162ccbdafae",
+    "url": "/static/js/main.c3be51d0.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
